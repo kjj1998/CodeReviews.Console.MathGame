25e3ee6309b9e25fc64db79e3d64bf1970e8f93c
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-08-17 22:56:59 +0800

    added menu

---
+++ Menu.cs
@@ -0,0 +1,15 @@
+namespace CodeReviews.Console.MathGame;
+
+public class Menu
+{
+    public static void DisplayMenu()
+    {
+        System.Console.WriteLine("Welcome to the Math Game!");
+        System.Console.WriteLine("Please select which game you want to play from the list below:");
+        System.Console.WriteLine("1. Addition - Press A to select this");
+        System.Console.WriteLine("2. Subtraction - Press B to select this");
+        System.Console.WriteLine("3. Multiplication - Press C to select this");
+        System.Console.WriteLine("4. Division - Press D to select this");
+        System.Console.WriteLine("5. Exit - Press E to end the game\n");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodeReviews.Console.MathGame;
+
+string? gameChoice;
+bool exitGame = false;
+HashSet<string> validGameChoices = ["A", "B", "C", "D", "E", "a", "b", "c", "d", "e"];
+
+do
+{
+    Menu.DisplayMenu();
+    gameChoice = Console.ReadLine();
+    while (gameChoice != null && !validGameChoices.Contains(gameChoice))
+    {
+        Console.WriteLine("Invalid input. Please re-enter your choice.");
+        gameChoice = Console.ReadLine();
+    }
+
+    if (gameChoice is "e" or "E")
+    {
+        exitGame = true;
+    }
+    else
+    {
+        switch (gameChoice)
+        {
+            case "a":
+                Console.WriteLine("You have selected the addition game!");
+                break;
+            case "b":
+                Console.WriteLine("You have selected the subtraction game!");
+                break;
+            case "c":
+                Console.WriteLine("You have selected the multiplication game!");
+                break;
+            case "d":
+                Console.WriteLine("You have selected the division game!");
+                break;
+            default:
+                Console.WriteLine("Invalid input. Please re-enter your choice");
+                break;
+        }
+    }
+} while (exitGame == false);
+
