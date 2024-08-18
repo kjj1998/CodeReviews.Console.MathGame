d42ecb9e4294a4e93515890158fac60725643559
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-08-18 08:47:37 +0800

    added code for addition game

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using CodeReviews.Console.MathGame;
+﻿using System.Globalization;
+using CodeReviews.Console.MathGame;
 
 string? gameChoice;
 bool exitGame = false;
 HashSet<string> validGameChoices = ["A", "B", "C", "D", "E", "a", "b", "c", "d", "e"];
+var numberGenerator = new Random();
 
 do
 {
@@ -24,7 +26,34 @@ do
         {
             case "a":
                 Console.WriteLine("You have selected the addition game!");
-                break;
+                Console.WriteLine("Enter your answer after the prompt or enter the letter 'e' to end the current game");
+                
+                while (true)
+                {
+                    int numA = numberGenerator.Next(0, 100);
+                    int numB = numberGenerator.Next(0, 100);
+                    int sum = numA + numB;
+                    
+                    Console.WriteLine($"What is {numA} + {numB} ?");
+                    string? response = Console.ReadLine();
+
+                    if (response != null && response.ToLower().Equals("e"))
+                    {
+                        break;
+                    }
+                    
+                    bool validAnswer = int.TryParse(response, out int answer);
+                    if (validAnswer == false)
+                    {
+                        Console.WriteLine($"{response} is invalid!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(answer == sum ? "Your answer is correct!" : "Your answer is wrong!");
+                    }
+                }
+                Console.WriteLine();
+                break;  
             case "b":
                 Console.WriteLine("You have selected the subtraction game!");
                 break;
